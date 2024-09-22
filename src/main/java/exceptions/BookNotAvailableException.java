cf2f625fd0382856d07acd673ddd4b705fd335f1
parsakav <parsakavianpour@gmail.com>
2024-09-22 14:33:17 +0330

    Implement BookNotFoundException as a runtime exception

---
+++ src/main/java/exceptions/BookNotAvailableException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+
+/***
+ * It occurs when the required book is not available
+ * It's a subclass of RuntimeException because we can prevent it with if-else condition
+ */
+public class BookNotAvailableException extends RuntimeException{
+
+}
